46d9df4f629a960c90a544dc9a1b4cd86ebc7c76
Lama <lamamamoun90@gmail.com>
2025-09-23 15:45:40 +0200

    Här använder jag Jotai för att hantera data som behövs i hela appen

---
+++ store/moodStore.ts
@@ -0,0 +1,7 @@
+// Här använder jag Jotai för att hantera data som behövs i hela appen
+
+import { atom } from 'jotai';
+import { MoodType, Place, UserLocation } from '../types/mood';
+
+// Atom för vilket humör användaren har valt
+export const selectedMoodAtom = atom<MoodType | null>(null);
