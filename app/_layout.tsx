094a1985907fed7fd62d1a429b0b4c4821573489
Lama <lamamamoun90@gmail.com>
2025-09-30 14:41:36 +0200

    Tog bort en onödig kommentar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-// Root layout som sätter upp Jotai Provider och grundläggande konfiguration
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
