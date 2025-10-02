820866ee789cd85bba7d3b5c955d69495c9822b4
Lama <lamamamoun90@gmail.com>
2025-10-02 12:58:53 +0200

    Tog bort onödiga kommentar

--- store/moodStore.ts
+++ store/moodStore.ts
@@ -1,18 +1,12 @@
 // Här använder jag Jotai för att hantera data som behövs i hela appen
-
 import { atom } from 'jotai';
 import { MoodType, Place, UserLocation } from '../types/mood';
 
 // Atom för vilket humör användaren har valt
 export const selectedMoodAtom = atom<MoodType | null>(null);
 
-// Atom för användarens nuvarande position
 export const userLocationAtom = atom<UserLocation | null>(null);
-
-// Atom för rekommenderade platser baserat på humör
 export const recommendedPlacesAtom = atom<Place[]>([]);
-
-// Atom för användarens favoritplatser (sparas med SecureStore)
 export const favoritePlacesAtom = atom<Place[]>([]);
 
 // Atom för om appen laddar data
